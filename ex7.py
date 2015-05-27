2ac23e35cc351890936995d4356b5ca9d3620fee
fragroute <fragroute@gmail.com>
2015-05-27 11:53:27 +0800

    add comments

--- ex7.py
+++ ex7.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-print "Mary had ad little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10 #what'd that do?
+print "Mary had ad little lamb." #This is a string
+print "Its fleece was white as %s." % 'snow'  #This used a formatted string
+print "And everywhere that Mary went." #This is a string
+print "." * 10 #what'd that do? #This will print 10 *
 
 end1 = "C"
 end2 = "h"
@@ -19,5 +19,5 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5
+print end1 + end2 + end3 + end4 + end5 #use plus to connect end1-5 variables
 print end7 + end8 + end9 + end10 + end11 +end12
