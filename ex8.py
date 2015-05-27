1cc4a2a3abbeb108b4dae6cb1c015714b720fde8
fragroute <fragroute@gmail.com>
2015-05-27 16:01:46 +0800

    add UTF-8 sample

--- ex8.py
+++ ex8.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
 
-formatter = "%r %r %r %r"
+formatter = "%s %s %s %s"
 
 print formatter % (1,2,3,4)
 print formatter % ("one","two","three","four")
 print formatter % (True,False,False,True)
 print formatter % (formatter,formatter,formatter,formatter)
-print formatter % ("I had this thing.","That you could type up right.","But it didn't sing.","So I said goodnight.")
+print formatter % ("我是中文.","That you could type up right.","But it didn't sing.","So I said goodnight.")
