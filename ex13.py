351c692e92b370018f4a354efa76b74fcfb1d576
fragroute <fragroute@gmail.com>
2015-06-02 17:56:33 +0800

    add ex13

--- ex13.py
+++ ex13.py
@@ -6,3 +6,5 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+age = raw_input("Please input your age:")
+print "Your age is: %r" % age
