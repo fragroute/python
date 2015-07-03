c7a8e1eff4dd62cabe3ef0f63d5ea7deab992a48
fragroute <fragroute@gmail.com>
2015-07-03 18:27:11 +0800

    code optimization

--- ex17.py
+++ ex17.py
@@ -6,18 +6,18 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" % (from_file, to_file)
 
 #We could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+#in_file = open(from_file)
+indata = open(from_file).read()
 
 print "The input file is %d bytes long." % len(indata)
 print "Does the output file exist? %r" % exists(to_file)
 print "Ready hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
-out_file = open(to_file,'w')
-out_file.write(indata)
+#out_file = open(to_file,'w')
+open(to_file,'w').write(indata)
 
 print "Alright, all done."
 
-out_file.close()
-in_file.close()
+#out_file.close()
+#in_file.close()
